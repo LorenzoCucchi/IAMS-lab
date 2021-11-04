919d3a7fc190af0e54672cdfab5a51ca68cc3131
LorenzoCucchi <lorenzo.cucchi@skywarder.eu>
2021-11-04 11:29:26 +0100

    plots

---
+++ functions/plotOrbit.m
@@ -0,0 +1,59 @@
+function plotOrbit(orbit,dth)
+
+% 3D orbit plot
+
+% ----------------------------------------------------------
+% Input arguments :
+% orbit [1,1] = a        semi - major axis [km]
+% orbit [1,2] = e              eccentricity [-]
+% orbit [1,3] = i           inclination [ rad ]
+% orbit [1,4] = OM                 RAAN [ rad ]
+% orbit [1,5] = om    pericenter anomaly [rad ]
+% orbit [1,6] = thi initial true anomaly [rad ]
+% orbit [1,7] = thf   final true anomaly [rad ]
+% dth [1 x1]     true anomaly step size [ rad ]
+% ----------------------------------------------------------
+
+
+[numberOrbits,~] = size(orbit)
+
+for j = 1:numberOrbits    
+    a = orbit(j,1);
+    e = orbit(j,2);
+    i = orbit(j,3);
+    OM = orbit(j,4);
+    om = orbit(j,5);
+    thi = orbit(j,6);
+    thf = orbit(j,7);
+    
+    
+    if thf <= thi % the  =r sign involves the case in which th0 == thf 
+        % ( convention : last path to plot , i.e. full orbit to play )
+        thf = thi + 2* pi;
+    end
+   
+    numTh = round (abs(thf -thi )/dth);
+    th_v = linspace (thi ,thf , numTh );
+    RR = zeros (3, length ( th_v ));
+    VV = zeros (3, length ( th_v ));
+
+    for k = 1: length ( th_v )
+        [R,V] = param2rv ( a, e, j, OM, om, th_v(k));
+        RR (:,k) = R;
+        VV (:,k) = V;
+    end
+
+    X = RR (1 ,:);
+    Y = RR (2 ,:);
+    Z = RR (3 ,:);
+    
+    V_X = VV (1 ,:);
+    V_Y = VV (2 ,:);
+    V_Z = VV (3 ,:);
+
+    V = sqrt (VV (1 ,:) .^2 + VV (2 ,:) .^2 + VV (3 ,:) .^2) ;
+  
+    plot3(X,Y,Z)
+    hold on
+
+end
--- functions/provaplot.m
+++ functions/provaplot.m
@@ -3,17 +3,11 @@ load('dataA24.mat')
 
 
 startTime = datetime(2020,6,02,8,23,0);
-stopTime1 = startTime + seconds(TO(1).delta_time(2));
-stopTime2 = stopTime1 + seconds(TO(2).delta_time(2));
-stopTime3 = stopTime2 + seconds(TO(3).delta_time(1));
-stopTime2 = stopTime3 + seconds(TO(3).delta_time(2));
+stopTime1 = startTime + days(1);
 
 sampleTime = 240;
 sc = satelliteScenario(startTime,stopTime1,sampleTime);
-sc2 = satelliteScenario(stopTime1,stopTime2,sampleTime);
-%sat = satellite(sc,[PI.a*1000 PI.a*1000],[PI.e PI.e],[rad2deg(PI.i) rad2deg(PI.i)],[rad2deg(PI.OM) rad2deg(PI.OM)],[rad2deg(PI.om) rad2deg(TO(1).om_f)],[rad2deg(PI.theta) rad2deg(TO(1).theta_2)])
-sat = satellite(sc,PI.a*1000,PI.e,rad2deg(PI.i),rad2deg(PI.OM),rad2deg(PI.om),rad2deg(PI.theta))
-sat2 = satellite(sc2,PI.a*1000,PI.e,rad2deg(PI.i),rad2deg(PI.OM),r,rad2deg(TO(1).theta_2))
+sat = satellite(sc,[PI.a*1000 PI.a*1000],[PI.e PI.e],[rad2deg(PI.i) rad2deg(PI.i)],[rad2deg(PI.OM) rad2deg(PI.OM)],[rad2deg(PI.om) rad2deg(TO(1).om_f)],[rad2deg(PI.theta) rad2deg(TO(1).theta_2)])
 show(sat2)
 satelliteScenarioViewer(sc2, 'CameraReferenceFrame','Inertial');
 groundTrack(sat,'LeadTime',3600)
